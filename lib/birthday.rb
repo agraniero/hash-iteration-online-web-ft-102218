cd7168fb0cd9e2999c55384f5f5f9b3792247e72
Anthony Graniero <ananfro@gmail.com>
2018-10-28 19:41:37 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,12 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+birthday_kids = {
+   "Timmy" => 9, 
+   "Sarah" => 6, 
+   "Amanda" => 27
+  }
 
 def happy_birthday(birthday_kids)
-  
+  birthday_kids.each do |kid, age|
+    puts "#{kid}: "
 end
 
 
